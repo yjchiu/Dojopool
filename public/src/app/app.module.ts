3e5a214d8066fdd1af86c4f494b8b6c32dba5900
wenmin <wenmin0111@gmail.com>
2017-06-27 13:18:27 -0700

    add user component

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -10,12 +10,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     DashboardComponent,
+    UserComponent,
   ],
   imports: [
     BrowserModule,
